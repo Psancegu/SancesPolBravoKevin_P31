75e7a811cc38b978e0b39e14af7615d8422cf937
KevinBravoLascano <profkinbl@gmail.com>
2025-05-20 12:19:02 +0200

    inicio

--- src/prog2/vistaGUI/AppCentralUB.java
+++ src/prog2/vistaGUI/AppCentralUB.java
@@ -1,4 +1,26 @@
 package prog2.vistaGUI;
 
-public class AppCentralUB {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AppCentralUB extends JFrame {
+    private JPanel inicio;
+    private JButton GestioComponents;
+    private JButton VisualitzarInfo;
+    private JButton FinalitzarDia;
+    private JButton guardaCarregaDades;
+
+    public AppCentralUB() {
+        GestioComponents.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
