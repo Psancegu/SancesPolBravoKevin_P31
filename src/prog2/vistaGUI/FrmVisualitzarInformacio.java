89a644cb22d7f6cd9534e5ff2a47345709b340e3
Psancegu <psancegu9@alumnes.ub.edu>
2025-05-22 19:52:44 +0200

    Pàgina de Visualtzació de text acabada i funciona al 100%

--- src/prog2/vistaGUI/FrmVisualitzarInformacio.java
+++ src/prog2/vistaGUI/FrmVisualitzarInformacio.java
@@ -3,12 +3,16 @@ package prog2.vistaGUI;
 import prog2.adaptador.Adaptador;
 
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FrmVisualitzarInformacio extends JDialog {
     private JPanel contentPane;
-    private JButton buttonOK;
-    private JButton buttonCancel;
+    private JButton btnTornar;
+    private JComboBox cmboxOpcionsVisualitzar;
+    private JButton btnVisualitzar;
+    private JScrollPane paneMostrar;
+    private JTextArea txtMostrar;
     private Adaptador adaptador;
 
     public FrmVisualitzarInformacio(Adaptador adaptador) {
@@ -18,7 +22,35 @@ public class FrmVisualitzarInformacio extends JDialog {
         setSize(400,300);
         setModal(true);
         setLocationRelativeTo(null);
+        txtMostrar.setEditable(false);
+        txtMostrar.setLineWrap(true);
+        txtMostrar.setWrapStyleWord(true);
 
 
+        btnTornar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        btnVisualitzar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String seleccio = cmboxOpcionsVisualitzar.getSelectedItem().toString();
+
+                switch (seleccio) {
+                    case "Estat de la Central":
+                        txtMostrar.setText(FrmVisualitzarInformacio.this.adaptador.mostrarEstat());
+                        break;
+                    case "Quadern de Bitàcola":
+                        txtMostrar.setText(FrmVisualitzarInformacio.this.adaptador.mostrarBitacola());
+                        break;
+                    case "Incidències":
+                        txtMostrar.setText(FrmVisualitzarInformacio.this.adaptador.mostrarIncidencies());
+                        break;
+                }
+                txtMostrar.setCaretPosition(0);
+            }
+        });
     }
 }
